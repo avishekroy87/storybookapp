95605879f1f4379b4d2f2549b9f7538b6d47c84d
Avishek roy <73749526+avishekroy87@users.noreply.github.com>
2022-10-30 23:09:35 +0530

    thisissecond

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import  '../../assets/css/style.css'
 
 function Menu(){
     return(
         <>
-            <ul style={{ color: "red", fontWeight: "bold" } }>
-                <li>Home</li>
-                <li>About us</li>
-                <img src= { require('../../assets/images/atom.png') } height={50} width={50}/>
-            </ul>
+        <div class="loader_bg">
+            <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+        </div>
+        <div id="mySidepanel" class="sidepanel">
+         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+         <a href="index.html">Home</a>
+         <a href="#about">About</a>
+         <a href="#we_Do">We Do</a>
+         <a href="#team">Our Team</a>
+         <a href="#testimonial">Testimonial</a>
+         <a href="#contact">Contact Us</a>
+      </div>
         </>
     )
 }
