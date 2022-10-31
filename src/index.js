8a2e7b731b735749127ffe341f2cdb7bd8923861
Avishek roy <73749526+avishekroy87@users.noreply.github.com>
2022-10-31 23:45:26 +0530

    template

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Menu from './components/menu';
 
+    import  'src/assets/css/bootstrap.css';
+
+    import  'src/assets/css/style.css';
+    import  'src/assets/css/responsive.css';
+    import  'src/assets/css/jquery.mCustomScrollbar.min.css;
+
+    
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen</link>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Menu />
