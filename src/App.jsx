092bd518c6f4dbf1b1a9a42a5d872707bcd83aaf
Avishek roy <73749526+avishekroy87@users.noreply.github.com>
2022-12-28 21:45:47 +0530

    secexpense

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Expenseitem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <Expenseitem />
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+function Expenseitem(){
+    return(
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    );
+}
+
+export default Expenseitem;
