ac432e8fe09781b85d9e8c243a65e81f8e56c541
Avishek <73749526+avishekroy87@users.noreply.github.com>
2023-05-09 22:05:15 +0530

    seccommit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React, { useState } from "react";
 
-
 export default function App() {
-  
   const [Firstnum, setFirstnum] = useState(0)
-  const [Secnum, setSecnum] = useState(0)
   const [res, setRes] = useState(0)
   
-
-  function addNum(){setRes ( parseInt (Firstnum) + parseInt (Secnum))}
-
-  const subNum = () => {setRes ( parseInt (Firstnum) - parseInt (Secnum))}
-
-  const prodNum = () => {setRes ( parseInt (Firstnum) * parseInt (Secnum))}
-
+  
+  const addNum = () => {   if(parseInt(res) !== '' || res !== 'Cannot divide by zero'){ setRes(parseInt(Firstnum) + parseInt(res)) }}
+  const subNum = () => { if(parseInt(res) !== '' || res !== 'Cannot divide by zero'){setRes(parseInt(Firstnum) - parseInt(res))} }
+  const prodNum = () => { if(parseInt(res) !== '' || res !== 'Cannot divide by zero'){setRes(parseInt(Firstnum) * parseInt(res))} }
   const adivNum = () => {
-      console.log('second nume', Secnum)
-      if(parseInt (Secnum) !== 0 ){
-            setRes ( parseInt (Firstnum) / parseInt (Secnum))
-      }else{
-          setSecnum(1);
-          setRes ( parseInt (Firstnum) / parseInt (Secnum));
-      }
-
-
+    if(parseInt(res) !== '' && parseInt(res) !== 0){ setRes(parseInt(Firstnum) / parseInt(res))}else{
+      setRes('Cannot divide by zero')
+    }
   }
 
 
 
-  const restInp = () => {setFirstnum(0); setSecnum(0);}
 
-  const restRes = () => {setRes(0)}
+  const restInp = () => { setFirstnum(0) }
+
+  const restRes = () => { setRes(0) }
   return (
     <div>
       <div className="result">{res}</div>
-      <input type="number" value={Firstnum} onChange={(e) => {setFirstnum(e.target.value)}}/>
-      <input type="number" value={Secnum} onChange={(e) => {setSecnum(e.target.value)}}/>
+      <input type="number" value={Firstnum} onChange={(e) => { setFirstnum(e.target.value) }} />
+      {/* <input type="number" value={Secnum} onChange={(e) => {setSecnum(e.target.value)}}/> */}
       <button onClick={addNum}>Add</button>
 
       <button onClick={subNum}>Sub</button>
@@ -44,12 +33,12 @@ export default function App() {
 
       <button onClick={adivNum}>Divide</button>
 
-      <button onClick={restInp} style={{color: 'red'}}>Reset input</button>
+      <button onClick={restInp} style={{ color: 'red' }}>Reset input</button>
 
-      <button onClick={restRes} style={{color: 'blue'}}>Reset res</button>
+      <button onClick={restRes} style={{ color: 'blue' }}>Reset res</button>
     </div>
 
   );
-  
+
 }
 
