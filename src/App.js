c696db93d1b3a9ac72174997401a2e3fab9c4354
Avishek <73749526+avishekroy87@users.noreply.github.com>
2023-05-09 21:36:53 +0530

    initcommit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+export default function App() {
+  
+  const [Firstnum, setFirstnum] = useState(0)
+  const [Secnum, setSecnum] = useState(0)
+  const [res, setRes] = useState(0)
+  
+
+  function addNum(){setRes ( parseInt (Firstnum) + parseInt (Secnum))}
+
+  const subNum = () => {setRes ( parseInt (Firstnum) - parseInt (Secnum))}
+
+  const prodNum = () => {setRes ( parseInt (Firstnum) * parseInt (Secnum))}
+
+  const adivNum = () => {
+      console.log('second nume', Secnum)
+      if(parseInt (Secnum) !== 0 ){
+            setRes ( parseInt (Firstnum) / parseInt (Secnum))
+      }else{
+          setSecnum(1);
+          setRes ( parseInt (Firstnum) / parseInt (Secnum));
+      }
+
+
+  }
+
+
+
+  const restInp = () => {setFirstnum(0); setSecnum(0);}
+
+  const restRes = () => {setRes(0)}
   return (
-    <div className="App">
-      <header className="App-header">
-        In header section
-      </header>
+    <div>
+      <div className="result">{res}</div>
+      <input type="number" value={Firstnum} onChange={(e) => {setFirstnum(e.target.value)}}/>
+      <input type="number" value={Secnum} onChange={(e) => {setSecnum(e.target.value)}}/>
+      <button onClick={addNum}>Add</button>
+
+      <button onClick={subNum}>Sub</button>
+
+      <button onClick={prodNum}>Multiply</button>
+
+      <button onClick={adivNum}>Divide</button>
+
+      <button onClick={restInp} style={{color: 'red'}}>Reset input</button>
+
+      <button onClick={restRes} style={{color: 'blue'}}>Reset res</button>
     </div>
+
   );
+  
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Menu from './components/menu';
 
-    import  'src/assets/css/bootstrap.css';
 
-    import  'src/assets/css/style.css';
-    import  'src/assets/css/responsive.css';
-    import  'src/assets/css/jquery.mCustomScrollbar.min.css;
-
-    
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen</link>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<Menu />
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App/>)
 reportWebVitals();
